7cae3767e61487a8bd4b222ef6808fee6b5a270e
luigi-guernelli <luigi.guernelli.corso@gmail.com>
2025-09-18 20:33:37 +0200

    Utilizzo della classe Scanner e creazione di un Menu ciclico

---
+++ 02-java-oop/03-interazioni-utente/p01_classe_scanner/src/classe_scanner/ScannerClass.java
@@ -0,0 +1,26 @@
+package classe_scanner;
+
+import java.util.Scanner;
+
+// Classe principlae
+public class ScannerClass {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Inserisci il tuo nome: ");
+		String nome = input.nextLine();
+
+		System.out.print("Inserisci la tua età: ");
+		int eta = input.nextInt();
+
+		if (eta >= 18) {
+			System.out.println("Ciao, " + nome + " sei maggiorenne!");
+		} else {
+			System.out.println("Ciao, " + nome + " sei minorenne!");
+		}
+		input.close();
+
+	}
+
+}
---
+++ 02-java-oop/03-interazioni-utente/p03_menu_ciclico/src/menu_ciclico/MenuCiclico.java
@@ -0,0 +1,59 @@
+package menu_ciclico;
+
+import java.util.Scanner;
+
+public class MenuCiclico {
+		public static void main(String[] args) {
+
+			// Creazione dello scanner
+			Scanner scanner = new Scanner(System.in);
+			
+			// Lettura scelta dall'utente
+						int scelta;
+
+			// Stampa del menu
+			do {
+				
+				System.out.println("===== MENU PRINCIPALE =====");
+				System.out.println("1. Saluta");
+				System.out.println("2. Calcola somma");
+				System.out.println("3. Esci");
+				System.out.print("Scegli un'opzione(1-3): ");
+				
+				scelta = scanner.nextInt();
+				
+				// Controlle della scelta
+				switch (scelta) {
+				case 1:
+					System.out.println("Ciao utente, benvenuto");
+					break;
+
+				case 2:
+					System.out.print("Inserisci il primo numero: ");
+					int numero1 = scanner.nextInt();
+
+					System.out.print("Inserisci il secondo numero: ");
+					int numero2 = scanner.nextInt();
+					
+					int somma = numero1 + numero2;
+					System.out.print("La somma è: " + somma);
+					break;
+
+				case 3:
+					System.out.println("Uscita dal programma . . . Arrivederici!");
+					break;
+
+				default:
+					System.out.println("Scelta non valida. Riprova.");
+
+				}
+				
+			} while (scelta != 3); // Ripeti finchè la scelta NON è 3
+
+
+			// Chiudiamo lo scanner
+			scanner.close();
+
+		}
+
+	}
