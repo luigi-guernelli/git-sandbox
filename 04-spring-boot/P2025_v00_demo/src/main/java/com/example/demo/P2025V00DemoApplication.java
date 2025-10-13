0fe9cb7f7ca04d9fe38ba289a49ba36031633070
luigi-guernelli <luigi.guernelli.corso@gmail.com>
2025-10-13 23:14:10 +0200

    Primo progetto demo (/hello) con il Framework Sprigboot

---
+++ 04-spring-boot/P2025_v00_demo/src/main/java/com/example/demo/P2025V00DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2025V00DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2025V00DemoApplication.class, args);
+	}
+
+}
